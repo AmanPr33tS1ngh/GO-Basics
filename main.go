38cf028eb09c9ca7e0f32d7ec40f9853d4f34651
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-02-22 23:47:50 +0530

    maps, arrays, slices and loops

--- main.go
+++ main.go
@@ -75,6 +75,9 @@ func main(){
 	fmt.Println(dVal, rVal)
 
 	var d1Val, r1Val, err1 = divisionResultAndRemainder(5, 0)
+
+	// if else conditionals
+
 	if err1!=nil{
 		err1.Error()
 	}else if(rVal == 0){
@@ -83,6 +86,7 @@ func main(){
 		fmt.Println("the d1Val is: %v and r1Val is : %v", d1Val, r1Val) // printf similar to c
 	}
 
+	//switch 
 
 	switch {
 		case err1!=nil:
@@ -101,6 +105,77 @@ func main(){
 			fmt.Println("remainder 1 or 2")
 		default:
 			fmt.Println("default") // printf similar to c
+	}
+
+	// arrays
+
+	var intArr [3]int32 // declaration of array
+	fmt.Println(intArr[0])
+	fmt.Println(intArr[1])
+	fmt.Println(intArr[0: 3])
+
+	fmt.Println(&intArr[0]) // returns address of stored val
+	fmt.Println(&intArr[1])
+
+	// another way to declare array
+	var intArray [3]int32 = [3]int32{1,2,3}
+	fmt.Println(intArray)
+
+	// another way to declare array with colon equal declaration method
+	intArrayWithCol := [3]int32{1,2,3}
+	fmt.Println(intArrayWithCol)
+
+	// another way to declare array with len not defining
+	intArrayWithNoLen := [...]int32{1,2,3}
+	fmt.Println(intArrayWithNoLen)
+	
+	// Slice
+	var intSlice []int32 = []int32{4,5,6} // declaration of slice
+	fmt.Println("after", intSlice, len(intSlice), cap(intSlice)) // returns length 3 and cap 3
+	intSlice = append(intSlice, 7)
+	fmt.Println("after", intSlice, len(intSlice), cap(intSlice)) // returns length 4 which is basically elements in an array and cap is new increased size of slice 
+
+	var intSlice2 []int32 = []int32{9,10,11}
+	intSlice = append(intSlice, intSlice2...) // another way of appending elements in a slice
+	fmt.Println(intSlice)
+
+
+	// looping in slices
+	for index, value := range intSlice{
+		fmt.Println(index, value)
+	}
+
+	// without index
+	for _, value := range intSlice{
+		fmt.Println(value)
+	}
+
+	var i int = 0 // while loop
+	for i<10{
+		fmt.Println(i)
+		i += 1
+	}
+
+	for i=0;i<10;i++{
+		fmt.Println(i)
+	}
+
+
+	// maps => similar to dict in python
+	var myMap map[string]uint8 = make(map[string]uint8) // declaration of map
+	fmt.Println(myMap)
+	var myMap2 = map[string]uint8{"age": 19}
+	fmt.Println(myMap2)
+	fmt.Println(myMap2["age"])
+
+	var myMap2Val, exists = myMap2["age"] // myMap2Val will be having value of myMap2["age"] item and exists is a boolean if value exists it will be true
+	if exists{
+		fmt.Println("value is", myMap2Val)
+	}
+
+	// looping in maps
+	for age, val := range myMap2{
+		fmt.Println(age, val)
 	}
 
 
